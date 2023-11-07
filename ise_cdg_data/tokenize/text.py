15ac5513834df4177e35a7f0485425cb9aac98e2
sepehrkianian09 <59197271+sepehrkianian09@users.noreply.github.com>
2023-11-07 14:58:13 +0330

    fix: fix minor bugs

--- ise_cdg_data/tokenize/text.py
+++ ise_cdg_data/tokenize/text.py
@@ -8,7 +8,7 @@ class TextTokenizer(TokenizerInterface):
     spacy_eng = spacy.load('en_core_web_lg')
 
     def __call__(self, text) -> typing.Sequence[str]:
-        return [tok.text.lower() for tok in self.spacy_eng.tokenizer(text)]
+        return [tok.text.lower() for tok in self.spacy_eng.tokenizer(text) if not tok.is_space]
 
 class MarkdownToTextTokenizer(TokenizerInterface):
     spacy_eng = spacy.load('en_core_web_lg')
