36374ab2374534be7021cd2eaa7115f3a3a62bc7
Ashkan Khademian <ashkan.khd.q@gmail.com>
2023-11-08 22:44:35 +0330

    fix: fix padding

--- ise_cdg_data/padding_collate.py
+++ ise_cdg_data/padding_collate.py
@@ -32,27 +32,27 @@ class CNN2RNNCollate:
 
     def __call__(self, batch):
         # pad for data -> pad for cnn
-        sources = [item[0] for item in batch]  # shape: (batch, image_size)
+        sources = [item[0] for item in batch]
         sources = self.__pad(
             self.source_pad_idx, sources
-        )  # shape: (batch, data_max_seq_length)
+        )  # shape: (data_max_seq_length, batch)
         sources = self.__pad_to_length(
             sources, self.source_expected_sequence_length
-        )  # shape: (batch, max_seq_length)
+        )  # shape: (max_seq_length, batch)
 
         # pad for data
         headers = [item[1] for item in batch]  # shape: (batch)
-        headers = self.__pad(self.header_pad_idx, headers)
+        headers = self.__pad(self.header_pad_idx, headers) # shape: (data_max_seq_length, batch)
 
         return (sources, headers)
 
     def __pad(self, pad_idx, texts):
-        return pad_sequence(texts, batch_first=True, padding_value=pad_idx)
+        return pad_sequence(texts, batch_first=False, padding_value=pad_idx)
 
     @classmethod
     def __pad_to_length(cls, raw_input, expected_length):
         # raw_input.shape: (..., raw_input_length)
-        raw_input_length = raw_input.shape[-1]
+        raw_input_length = raw_input.shape[0]
         pad_length = expected_length - raw_input_length
         # shape: (..., expected_length)
-        return F.pad(raw_input, (0, pad_length), value=0)
+        return F.pad(raw_input, (0, 0, 0, pad_length), value=0)
