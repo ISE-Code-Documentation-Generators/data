6fab4e09f9d2abced02dd0a7fb6c842d883d8453
Ashkan Khademian <ashkan.khd.q@gmail.com>
2023-12-26 14:29:42 +0330

    fix: add batch_size to the interface module

--- ise_cdg_data/preprocess/interface.py
+++ ise_cdg_data/preprocess/interface.py
@@ -11,6 +11,7 @@ class PreprocessInterface(ABC):
 def get_preprocessor_for_ast_augmentation(
     path,
     batch_number: int = 0,
+    batch_size: int = 1000,
 ) -> PreprocessInterface:
     from ise_cdg_data.preprocess.preprocessor import ASTPreprocessor
-    return ASTPreprocessor(path, batch_number, batch_size=1000)
+    return ASTPreprocessor(path, batch_number=batch_number, batch_size=batch_size)
