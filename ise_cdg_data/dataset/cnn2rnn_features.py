1a4357f3e01b6bed6b8444343732b1fc3aa9d360
sepehrkianian09 <59197271+sepehrkianian09@users.noreply.github.com>
2024-01-29 00:15:13 +0330

    fix(cnn-rnn-features)

--- ise_cdg_data/dataset/cnn2rnn_features.py
+++ ise_cdg_data/dataset/cnn2rnn_features.py
@@ -50,8 +50,7 @@ class CNN2RNNFeaturesDatasetWithPreprocess(Md4DefDatasetInterface):
         self.filter_df()
         if compute_features:
             self.df[self.features_column] = get_source_features_extractor().extract(self.df['source'])
-        else:
-            self.df[self.features_column] = self.df[self.features_column].apply(eval)
+        self.df[self.features_column] = self.df[self.features_column].apply(eval)
         
         self.df = self.df[[self.source_column, self.md_column, self.features_column]]
 
